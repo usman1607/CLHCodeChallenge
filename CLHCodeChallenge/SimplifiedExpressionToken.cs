e30710da3c05a3e036306b8300e7c2f29e173bf8
usman1607 <usman1607@yahoo.com>
2022-01-07 12:36:17 +0100

    commit

--- CLHCodeChallenge/SimplifiedExpressionToken.cs
+++ CLHCodeChallenge/SimplifiedExpressionToken.cs
@@ -8,13 +8,40 @@ namespace CLHCodeChallenge
 {
     public class SimplifiedExpressionToken
     {
-        public static string ExpressionToken(string exp, string vars, int ints)
+        public static string ExpressionToken(string expression, string vars, int ints)
         {
+            expression.Replace(" ", "");
+
+            var exp = expression.Split(" ");
             var token = new StringBuilder();
+            var result = 0;
+            //var lastNum = 0;
+            var count = 0;
+
+            for(int i = 0; i < exp.Length; i++)
+            {
+                int num = 0;
+                if (exp[i].Equals(vars))
+                {
+                    count++;
+                }
+                else if (int.TryParse(exp[i], out num))
+                {
+                    if (i != 0 && exp[i - 1].Equals("+")) result += num;
+
+                    if (i != 0 && exp[i - 1].Equals("-")) result -= num;
+
+                    if (i != 0 && exp[i - 1].Equals("-")) result -= num;
+                }
+                else if (exp[i].Equals(""))
+                {
+
+                }
+            }
 
 
 
-            return token.ToString();
+            return $"[\"{token}\", \"{result}\"]";
         }
     }
 }
