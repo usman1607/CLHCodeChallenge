7d8fb760ac932e9bfbf254aef434dcfc91c50b5f
usman1607 <usman1607@yahoo.com>
2022-01-07 12:03:42 +0100

    ...commit 07-01-2022

---
+++ CLHCodeChallenge/SimplifiedExpressionToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLHCodeChallenge
+{
+    public class SimplifiedExpressionToken
+    {
+        public static string ExpressionToken(string exp, string vars, int ints)
+        {
+            var token = new StringBuilder();
+
+
+
+            return token.ToString();
+        }
+    }
+}
