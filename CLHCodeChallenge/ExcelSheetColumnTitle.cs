f171c4ebdd397611706977d1ced5d238f33cdf38
usman1607 <usman1607@yahoo.com>
2023-09-07 12:14:40 +0100

    explain

--- CLHCodeChallenge/ExcelSheetColumnTitle.cs
+++ CLHCodeChallenge/ExcelSheetColumnTitle.cs
@@ -40,7 +40,17 @@ namespace CLHCodeChallenge
 
             Constraints:
 
-            1 <= columnNumber <= 231 - 1*/
+            1 <= columnNumber <= 231 - 1
+        
+
+        Solution explanation.
+        ========================================
+        Suppose we have a number n, let’s say 28. so corresponding to it we need to print the column name. We need to take the remainder with 26. 
+
+        If the remainder with 26 comes out to be 0 (meaning 26, 52, and so on) then we put ‘Z’ in the output string and new n becomes n/26 -1 because here we are considering 26 to be ‘Z’ while in actuality it’s 25th with respect to ‘A’.
+
+        Similarly, if the remainder comes out to be non-zero. (like 1, 2, 3, and so on) then we need to just insert the char accordingly in the string and do n = n/26.
+         */
 
         public static string Solution(int columnNumber)
         {
